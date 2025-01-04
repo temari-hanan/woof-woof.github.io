476574b3b44f6bd25b6b21789646c2c733a2f5f8
nagashima.satoshi <abc123def567ghi890@yahoo.co.jp>
2025-01-05 00:05:52 +0900

    TESTで初期コミット

---
+++ resource.js
@@ -0,0 +1,6 @@
+// それぞれの名前にシングルかダブルクォートがあれば調整すること
+function initSongs() {
+    return [
+                {song_detail: '39みゅーじっく! / みきとP feat. 初音ミク',youtubeLinks: ['https://www.youtube.com/embed/DWIQrnssvtg?start=696','https://www.youtube.com/embed/BAOHCnO-61U?start=2196','https://www.youtube.com/embed/wYuPp0_IEzw?start=757',]},
+    ];
+}
